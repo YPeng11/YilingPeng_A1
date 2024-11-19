6b632b8947fbf901e1ac0d0235785f484d4203a1
YPeng11 <1306821402@qq.com>
2024-11-19 16:49:29 +0800

    GeneralPractitioner class Modification of the output field
    Print error changes

--- src/com/appointmentsystem/HealthProfessional.java
+++ src/com/appointmentsystem/HealthProfessional.java
@@ -5,7 +5,7 @@ public class HealthProfessional {
     protected int HealthProfessional_ID;
     protected String HealthProfessional_name;
     protected double price;
-    protected String specialization;
+    protected String PrimaryType;
     protected String place;
 
     // 默认构造函数
@@ -14,11 +14,11 @@ public class HealthProfessional {
 
     // 带参数的构造函数,初始化所有实例变量
     public HealthProfessional(int HealthProfessional_ID, String HealthProfessional_name, double price,
-            String specialization, String place) {
+            String PrimaryType, String place) {
         this.HealthProfessional_name = HealthProfessional_name;
         this.HealthProfessional_ID = HealthProfessional_ID;
         this.price = price;
-        this.specialization = specialization;
+        this.PrimaryType = PrimaryType;
         this.place = place;
     }
 
@@ -27,7 +27,7 @@ public class HealthProfessional {
         System.out.println("ID: " + HealthProfessional_ID);
         System.out.println("Name: " + HealthProfessional_name);
         System.out.println("Price: " + price);
-        System.out.println("Specialization: " + specialization);
+        System.out.println("Primary Type: " + PrimaryType);
         System.out.println("Place: " + place);
     }
 
